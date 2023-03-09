ff3b3b68d299f02eb0167977ee16413fecc903e1
Divya Sree Kuchipudi <kuchipudidivyachowdary123@gmail.com>
2023-03-08 23:06:48 -0600

    initial commit

---
+++ Project1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Kuchipudi Divya Sree on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
